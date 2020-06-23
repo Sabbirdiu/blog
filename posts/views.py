49e7ba9bc6db9cb6fada5029925250934e7e6d5b
Sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-06-23 20:37:48 +0600

    update the views for blog post ordering  by last post

--- posts/views.py
+++ posts/views.py
@@ -49,7 +49,7 @@ def post_list(request):
     categories = Category.objects.all()
     category_count = get_category_count()
     most_recent = Post.objects.order_by('-timestamp')[:3]
-    post_list = Post.objects.all()
+    post_list = Post.objects.order_by('-timestamp')
     paginator = Paginator(post_list, 8)
     page_request_var = 'page'
     page = request.GET.get(page_request_var)

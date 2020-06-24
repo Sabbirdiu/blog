4d03b30bed26cb7a8787426016ffbcb24583f6d2
Sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-06-24 20:33:54 +0600

    update the index templates for catagories update

--- posts/views.py
+++ posts/views.py
@@ -24,6 +24,8 @@ def index(request):
     featured = Post.objects.filter(featured=True)
     latest = Post.objects.order_by('-timestamp')[0:3]
     popular_posts = Post.objects.order_by('-hit_count__hits')[:3]
+    categories = Category.objects.filter()
+    category_count = get_category_count()
     if request.method == "POST":
         email = request.POST["email"]
         new_signup = Signup()
@@ -32,7 +34,9 @@ def index(request):
     context = {
         'object_list':featured,
         'latest':latest,
-        'popular_posts':popular_posts
+        'popular_posts':popular_posts,
+        'categories':categories,
+        'category_count':category_count
     }
     return render(request, 'index.html',context)
 

7fa5ae24813611f88a1c39216d4cb2f33ee51888
Sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-07-05 10:24:16 +0600

    added the comment model and admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from .models import Author,Category, Post,About
+from .models import Author,Category, Post,About,Comment
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
     # list_display = ('title')
     prepopulated_fields = {'slug': ('title',)}
 class CatAdmin(admin.ModelAdmin):
     # list_display = ('title')
-    prepopulated_fields = {'slug': ('title',)}    
+    prepopulated_fields = {'slug': ('title',)}  
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin): 
+    list_display = ('name', 'email', 'post', 'created', 'active') 
+    list_filter = ('active', 'created', 'updated') 
+    search_fields = ('name', 'email', 'body') 
+
 admin.site.register(Author)
 
 admin.site.register(Category,CatAdmin)
--- posts/models.py
+++ posts/models.py
@@ -68,6 +68,22 @@ class Post(models.Model):
         return reverse('post-detail', kwargs={
             'slug': self.slug
         })
+class Comment(models.Model): 
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80) 
+    email = models.EmailField() 
+    body = models.TextField() 
+    created = models.DateTimeField(auto_now_add=True) 
+    updated = models.DateTimeField(auto_now=True) 
+    active = models.BooleanField(default=True) 
+ 
+    class Meta: 
+        ordering = ('created',) 
+ 
+    def __str__(self): 
+        return 'Comment by {} on {}'.format(self.name, self.post)        
 class About(models.Model):
     overview = RichTextUploadingField()
     image = models.ImageField(upload_to='uploads/')        
